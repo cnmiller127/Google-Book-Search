0a5bb97d10ac7d106c7a89d1e8b2586f2ba27acd
Christian Miller <cnmiller127@gmail.com>
2020-08-20 16:20:32 -0500

    rm unnecesarry comments

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -55,28 +55,4 @@ function NavExp() {
     );
 }
 
-
-  
-
-  
-
-
 export default NavExp;
-
-
-  //   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  //   <a className="navbar-brand" href="/">Library</a>
-  //   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-  //     <span className="navbar-toggler-icon"></span>
-  //   </button>
-  //   <div className="collapse navbar-collapse" id="navbarNav">
-  //     <ul className="navbar-nav">
-  //       <li className = {`nav-item ${(navStatus.status === "home") ? "active" : ""}`} >
-  //         <a className= "nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-  //       </li>
-  //       <li className= {`nav-item ${(navStatus.status === "lib") ? "active" : ""}`}>
-  //         <a className="nav-link" href="/mylibrary">My Library</a>
-  //       </li>
-  //     </ul>
-  //   </div>
-  // </nav>
